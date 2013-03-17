f7ed45f243d3e21db23c5e0c975d85b9b27b2fd7
Dmitry Shirokov <deadrunk@gmail.com>
2013-03-17 17:13:06 +0400

    dynamic buffer class, perf optimizations

--- benchmark.js
+++ benchmark.js
@@ -1,21 +1,28 @@
 
+var fs = require('fs');
 var ls = require('./lib/lookup_service');
 
 
 ls.init('./test/dbs/GeoLiteCity.dat');
+var ips = fs.readFileSync('./test/dbs/ips.txt');
 
+ips = ips.toString().split("\n").map(function(line) {
+    return line.trim();
+});
 
-var n = 500000;
+var n = ips.length;
 var s = new Date().getTime();
-for (var i = 0; i < n; i++) {
-    ls.getLocation([
-        Math.round(Math.random() * 254),
-        Math.round(Math.random() * 254),
-        Math.round(Math.random() * 254),
-        Math.round(Math.random() * 254)
-    ].join('.'));
+
+for (var i = ips.length - 1; i >= 0; i--) {
+    ls.getLocation(ips[i]);
 };
 
 console.log('n: ', n);
 console.log('time:', (new Date().getTime() - s), "msec");
 console.log('speed:', Math.round(n / (new Date().getTime() - s) * 1000), 'per sec');
+
+// console.log(ls.dump())
+
+// var time = process.hrtime();
+// var diff = process.hrtime(time);
+// console.log((diff[0] * 1e9 + diff[1]) / 1e9);
