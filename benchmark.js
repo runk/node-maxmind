f30c85b844ef880016a5158b25238927305a72fc
Dmitry Shirokov <deadrunk@gmail.com>
2013-10-11 14:13:49 +1100

    performance tuning

--- benchmark.js
+++ benchmark.js
@@ -1,28 +1,20 @@
 
-var fs = require('fs');
 var ls = require('./lib/lookup_service');
 
 function randip() {
-    return [
-        Math.floor(Math.random() * 255),
-        Math.floor(Math.random() * 255),
-        Math.floor(Math.random() * 255),
-        Math.floor(Math.random() * 255)
-    ].join('.');
+  return Math.floor(Math.random() * 255) + '.' +
+    Math.floor(Math.random() * 255) + '.' +
+    Math.floor(Math.random() * 255) + '.' +
+    Math.floor(Math.random() * 255);
 }
 
 ls.init('./test/dbs/GeoIPCity.dat', { memoryCache: true });
-var ips = fs.readFileSync('./test/dbs/full/ips.txt');
-ips = ips.toString().split("\n").map(function(line) {
-    return line.trim();
-});
 
-var n = ips.length;
 var s = new Date().getTime();
+var n = 1000000;
 
-for (var i = ips.length - 1; i >= 0; i--) {
-    ls.getLocation(ips[i]);
-};
+for (var i = 0; i < n; i++)
+  ls.getLocation(randip())
 
 console.log('n: ', n);
 console.log('time:', (new Date().getTime() - s), "msec");
