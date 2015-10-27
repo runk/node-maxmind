b9560cbe73d4961ff088c5a85b98c0c642ac41ae
mbelev <martinbelev93@gmail.com>
2015-10-28 00:12:03 +0200

    Better naming. Remove redundant code.
    Added comment with location object properties.

--- examples/asn.js
+++ examples/asn.js
@@ -1,4 +1,4 @@
-
 var maxmind = require('maxmind');
 maxmind.init(__dirname + '/GeoIPASNum.dat');
-console.log(maxmind.getOrganization('66.6.44.4'));
+
+console.log('ASN:', maxmind.getOrganization('66.6.44.4'));
--- examples/city.js
+++ examples/city.js
@@ -1,16 +1,7 @@
-
 var maxmind = require('maxmind');
 maxmind.init(__dirname + '/GeoLiteCity.dat');
 
-var l = maxmind.getLocation("66.6.44.4");
-
-console.log('countryCode\t', l.countryCode);
-console.log('countryName\t', l.countryName);
-console.log('region\t\t', l.region);
-console.log('city\t\t', l.city);
-console.log('postalCode\t', l.postalCode);
-console.log('latitude\t', l.latitude);
-console.log('longitude\t', l.longitude);
-console.log('metroCode\t', l.metroCode);
-console.log('dmaCode\t', l.dmaCode);
-console.log('areaCode\t', l.areaCode);
+// The returned location object has the following properties:
+// countryCode, countryName, region, city, latitude, longitude
+// postalCode, metroCode, dmaCode, areaCode
+console.log('Location:', maxmind.getLocation('66.6.44.4'));
