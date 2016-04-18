39086adaf06660c60a54a1a695d1f32f3c4ecc9b
Dmitry Shirokov <deadrunk@gmail.com>
2016-04-18 20:31:49 +1000

    Decoder test

--- test/e2e.js
+++ test/e2e.js
@@ -4,7 +4,6 @@ var mmdbreader = require('../index');
 var ipaddr = require('ip-address');
 
 
-
 var actual = function(file, subnetKey) {
   var data = require('./data/source-data/' + file);
   var hash = {};
@@ -54,7 +53,46 @@ describe('maxmind', function() {
     });
   });
 
-  describe('data files', function() {
+  describe('section: data', function() {
+    it('should decode all possible types - complex', function() {
+      var geoIp = mmdbreader.openSync(__dirname + '/data/test-data/MaxMind-DB-test-decoder.mmdb');
+      assert.deepEqual(geoIp.getGeoDataSync('::1.1.1.1'), {
+        array: [1, 2, 3],
+        boolean: true,
+        bytes: new Buffer([0, 0, 0, 42]),
+        double: 42.123456,
+        // It should be 1.1, but there's some issue with rounding in v8
+        float: 1.100000023841858,
+        int32: -268435456,
+        map: { mapX: { arrayX: [7, 8, 9], utf8_stringX: 'hello' } },
+        uint128: '1329227995784915872903807060280344576',
+        uint16: 100,
+        uint32: 268435456,
+        uint64: '1152921504606846976',
+        utf8_string: 'unicode! ☯ - ♫',
+      })
+    });
+
+    it('should decode all possible types - zero/empty values', function() {
+      var geoIp = mmdbreader.openSync(__dirname + '/data/test-data/MaxMind-DB-test-decoder.mmdb');
+      assert.deepEqual(geoIp.getGeoDataSync('::0.0.0.0'), {
+        array: [],
+        boolean: false,
+        bytes: new Buffer([]),
+        double: 0,
+        float: 0,
+        int32: 0,
+        map: {},
+        uint128: '0',
+        uint16: 0,
+        uint32: 0,
+        uint64: '0',
+        utf8_string: '',
+      });
+    });
+  });
+
+  describe('section: binary search tree', function() {
 
     var files = [
       'GeoIP2-Anonymous-IP-Test',
