dfff43aaccd9d6641d7f8903a29787b0f309d414
antishov <i.am.r0ocket@gmail.com>
2017-06-11 21:48:11 +0300

    Added handler for exceptions from Reader's constructor

--- index.js
+++ index.js
@@ -19,7 +19,15 @@ exports.open = function(filepath, opts, cb) {
     if (isGzip(database)) {
       return cb(new Error('Looks like you are passing in a file in gzip format, please use mmdb database instead.'));
     }
-    var reader = new Reader(database, opts);
+
+    var reader;
+
+    try {
+      reader = new Reader(database, opts);
+    } catch (err) {
+      return cb(err);
+    }
+
     if (opts && !!opts.watchForUpdates) {
       fs.watch(filepath, function() {
         fs.readFile(filepath, function(err, database) {
