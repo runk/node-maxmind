0c0b35f8e8ad7e539f2710925442dc6d41fba650
rishabhbits038 <rishabh.bits038@gmail.com>
2019-05-15 16:35:52 +0530

    changed min node version in assert message

--- src/decoder.ts
+++ src/decoder.ts
@@ -6,7 +6,7 @@ import utils from './utils';
 
 assert(
   typeof BigInt !== 'undefined',
-  'Apparently you are using old version of node. Please upgrade to node 10.x or above.'
+  'Apparently you are using old version of node. Please upgrade to node 10.4.x or above.'
 );
 
 const types = [
